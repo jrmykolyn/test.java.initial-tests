3ea1edfc1040de8eaa289fe00ac3734850a7dd6a
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-06-29 20:51:37 -0400

    Add data type example: char

---
+++ src/data-types/DataTypes.java
@@ -0,0 +1,11 @@
+class DataTypes {
+  static public void main(String[] args) {
+    // Java supports two types of data:
+    // - Primitive.
+    // - Non-primitive.
+
+    // The following are examples of primitive data.
+    char a = 'A';
+    System.out.println(a);
+  }
+}
