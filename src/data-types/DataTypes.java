6a83c4a964dea78c0a834a10730d37a81c85158e
Jesse Mykolyn <jrmykolyn@gmail.com>
2019-07-04 21:04:14 -0400

    Add data type example: long

--- src/data-types/DataTypes.java
+++ src/data-types/DataTypes.java
@@ -19,5 +19,11 @@ class DataTypes {
     // The 'int' type is used to store numeric values up to 4 bytes in length.
     int z = 65536;
     System.out.println(z);
+
+    // The 'long' type is used to store numeric values up to 8 bytes in length.
+    // This includes numbers as large as 2,147,438,647, and as small as
+    // -2,147,438,658.
+    long zz = 2147483647;
+    System.out.println(zz);
   }
 }
